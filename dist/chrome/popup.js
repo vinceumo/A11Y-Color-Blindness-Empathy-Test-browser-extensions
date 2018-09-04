5f0dfd650378743e5e5a8ab963f601bcc768f36d
Vincent Humeau <hello@vincent-humeau.com>
2018-09-04 14:07:44 +0100

    access DOM

---
+++ dist/chrome/popup.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function (evt) {
+	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+		file: 'payload.js'
+	});;
+});
+
+// Listen to messages from the payload.js script and write to popout.html
+chrome.runtime.onMessage.addListener(function (message) {
+	document.getElementById('pagetitle').innerHTML = message;
+});
