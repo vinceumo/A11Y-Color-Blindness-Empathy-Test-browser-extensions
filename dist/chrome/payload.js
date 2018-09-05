58769cffbb8dbb9a1a19bbaa3a94258cd1c2244c
Vincent Humeau <hello@vincent-humeau.com>
2018-09-05 18:21:42 +0100

    firefox test

--- dist/chrome/payload.js
+++ dist/chrome/payload.js
@@ -1,2 +1,3 @@
 // send the page title as a chrome message
 chrome.runtime.sendMessage(document.title);
+
--- dist/chrome/popup.js
+++ dist/chrome/popup.js
@@ -1,10 +1,26 @@
-window.addEventListener('load', function (evt) {
-	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
-		file: 'payload.js'
-	});;
-});
+// window.addEventListener('load', function (evt) {
+// 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+// 		file: 'payload.js'
+// 	});;
+// });
 
-// Listen to messages from the payload.js script and write to popout.html
-chrome.runtime.onMessage.addListener(function (message) {
-	document.getElementById('pagetitle').innerHTML = message;
+// // Listen to messages from the payload.js script and write to popout.html
+// chrome.runtime.onMessage.addListener(function (message) {
+// 	document.getElementById('pagetitle').innerHTML = message;
+// });
+
+var buttonTest = document.getElementById('testButton');
+//console.log(buttonTest);
+
+var someVar = {text: 'test', foo: 1, bar: false};
+chrome.tabs.executeScript({
+    code: '(' + function(params) {
+        document.body.insertAdjacentHTML('beforeend',
+            '<div style="all:unset; position:fixed; left:0; top:0; right:0; bottom:0;' +
+                'background-color:rgba(0,255,0,0.3)">' + params.foo + '</div>'
+        );
+        return {success: true, html: document.body.innerHTML};
+    } + ')(' + JSON.stringify(someVar) + ');'
+}, function(results) {
+    console.log(results[0]);
 });
